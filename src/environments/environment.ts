e6add8bb9f6b1d38d4a182bef995c82ddda30146
VVISHALGOUR0001 <vishalgour0020@gmail.com>
2023-06-06 17:34:59 +0530

    api.srevice/app.components.ts/tsconfig.json/app.commponent.html

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBCQjEFxP2smG5wbMWgTHu4pqmJoz5O8B4",
+    authDomain: "learning-angular-firebas-ca659.firebaseapp.com",
+    projectId: "learning-angular-firebas-ca659",
+    storageBucket: "learning-angular-firebas-ca659.appspot.com",
+    messagingSenderId: "92836613445",
+    appId: "1:92836613445:web:7651dddeaa71ed1c29822c",
+  },
+};
